9fa0d5c4778b551a8d2f505a963d3395fcf359ca
Jansen Penido <jansen.penido@gmail.com>
2017-12-19 22:31:32 -0200

    Converting code to Python 3.

--- monty_hall.py
+++ monty_hall.py
@@ -1,6 +1,12 @@
+"""
+Pulsar Image Stacking
+---------------------
+Author: Jansen Penido <jansen.penido@gmail.com>
+"""
+
 import sys,random
 
-print "Welcome to Monty Hall!"
+print("Welcome to Monty Hall!")
 
 random.seed()
 
@@ -9,9 +15,9 @@ lost = 0
 choice = 0
 games = 100000
 
-print "Would you like to swap doors?"
-print "[Y]es or [N]o: ",
-c = raw_input()
+print("Would you like to swap doors?")
+print("[Y]es or [N]o: "),
+c = input()
 
 if c.upper() == 'Y':
   swap = True
@@ -25,15 +31,15 @@ for i in range(games):
     random.shuffle(doors)
 
     choice = random.randrange(3)
-    
+
     for i, val in enumerate(doors):
         if doors[i] == "goat" and i != choice:
             presenter = i
             break
-    
+
     if swap == True:
         choice = random.randrange(3)
-        
+
         while( choice == presenter ):
             choice = random.randrange(3)
 
@@ -42,11 +48,11 @@ for i in range(games):
     else:
         lost += 1
 
-print "Time is up! Here are the results:"
+print("Time is up! Here are the results:")
 
 win_percent = "{0:.2f}%".format(float(won) / float(games) * 100)
 lost_percent = "{0:.2f}%".format(float(lost) / float(games) * 100)
 
 
-print "Wins: " + win_percent + " (" + str(won) + " times)"
-print "Losses: " + lost_percent + " (" + str(lost) + " times)"
+print("Wins: " + win_percent + " (" + str(won) + " times)")
+print("Losses: " + lost_percent + " (" + str(lost) + " times)")
